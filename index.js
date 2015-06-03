6b7125a1cbe7ef1338f550635e9d099b9d9d8c25
Alex Cordonnier <alexjcord@gmail.com>
2015-06-02 20:04:52 -0700

    Debugging

--- index.js
+++ index.js
@@ -76,7 +76,7 @@ $("#show-scoreboard").on("click", function() {
         "width=800, height=600");
 
     //Insert the correct stats once the popup loads
-    $(scoreboardWindow.document).on("load", function() {
+    $(scoreboardWindow.document).ready(function() {
         updateScoreboard();
     });
 });
