1e880881897cad776d7814cd85e6e0a8bb65be30
Alex Cordonnier <alexjcord@gmail.com>
2015-06-02 19:37:09 -0700

    Started scoreboard window

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 var homeScore = 0;
 var visitorScore = 0;
 
-var startingTimer = 0; //The original time on the clock
+var startingTimer = 10; //The original time on the clock
 var timer = 0; //The time on the clock
 var start = 0; //The starting timestamp
+var interval; //Stores the interval timer ID
 
 var round = 1; //The current round
-var interval; //Stores the interval timer ID
+
+var scoreboardWindow; //Stores the scoreboard window
+
+$("#home-score").on("change", function() {
+    homeScore = $("#home-score").val();
+});
 
 $("#home-plus-1").on("click", function() {
     homeScore++;
@@ -16,6 +22,10 @@ $("#home-plus-1").on("click", function() {
 $("#home-minus-1").on("click", function() {
     homeScore--;
     $("#home-score").val(homeScore);
+});
+
+$("#visitor-score").on("change", function() {
+    visitorScore = $("#visitor-score").val();
 });
 
 $("#visitor-plus-1").on("click", function() {
@@ -34,22 +44,27 @@ $(".round").on("click", function() {
     round = $(this).index() + 1;
 });
 
+$("#show-scoreboard").on("click", function() {
+    scoreboardWindow = window.open("scoreboard.html", "scoreboardWindow",
+        "width=800, height=600");
+});
+
 $("#timer-10").on("click", function() {
     window.clearInterval(interval);
     timer = 10;
-    startingTimer = 10;
+    startingTimer = timer;
     $("#timer").text(timer.toFixed(2));
     $("#timer-pause").hide();
-    $("#timer-start").show();
+    $("#timer-start").show().removeAttr("disabled");
 });
 
 $("#timer-30").on("click", function() {
     window.clearInterval(interval);
     timer = 30;
-    startingTimer = 30;
+    startingTimer = timer;
     $("#timer").text(timer.toFixed(2));
     $("#timer-pause").hide();
-    $("#timer-start").show();
+    $("#timer-start").show().removeAttr("disabled");
 });
 
 $("#timer-start").on("click", function() {
@@ -69,8 +84,8 @@ $("#timer-reset").on("click", function() {
     timer = startingTimer;
     $("#timer").text(timer.toFixed(2));
     $("#timer-pause").hide();
-    $("#timer-start").show();
-})
+    $("#timer-start").show().removeAttr("disabled");
+});
 
 
 function startTimer() {
@@ -83,9 +98,10 @@ function startTimer() {
 
         if (remaining < 0.005) {
             window.clearInterval(interval);
+            timer = 0;
             $("#timer").text("0.00");
             $("#timer-pause").hide();
-            $("#timer-start").show();
+            $("#timer-start").show().attr("disabled", "true");
         }
     }, 25);
 }
